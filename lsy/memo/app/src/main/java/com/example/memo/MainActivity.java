d1fc42df8b91ff583ccc608de0f9efad73066e24
2761383195 <87225072+2761383195@users.noreply.github.com>
2023-04-07 21:43:29 +0800

    修改了部分变量和语法

--- lsy/memo/app/src/main/java/com/example/memo/MainActivity.java
+++ lsy/memo/app/src/main/java/com/example/memo/MainActivity.java
@@ -1,45 +1,35 @@
 package com.example.memo;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    public ArrayList list=new ArrayList();
+    public ArrayList<String> text_edit_list=new ArrayList<String>();
     private ArrayAdapter<String> myadapter;
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         //悬浮窗按钮
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {//点击悬浮窗按钮进入下一个页面
-                String data=null;
-                Intent intent=new Intent(MainActivity.this,input_activity.class);
-                intent.putExtra("extra_data",data);
-                startActivityForResult(intent,1);
+                String input_data=null;
+                Intent intent_fab=new Intent(MainActivity.this,text_edit_activity.class);
+                intent_fab.putExtra("extra_data",input_data);
+                startActivityForResult(intent_fab,1);//打开下一个界面并传入唯一标识符1
             }
         });
 
@@ -52,20 +42,20 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode){
             case 1:
                 if(resultCode==RESULT_OK){
-                    String returndata=data.getStringExtra("data_return");
-                        list.add(returndata);
+                    String returndata=data.getStringExtra("data_return");//获得用户输入的数据
+                    text_edit_list.add(returndata);
                     ListView listView=(ListView) findViewById(R.id.list_view);
-                    myadapter=new ArrayAdapter<>(MainActivity.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,list);
+                    myadapter=new ArrayAdapter<>(MainActivity.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,text_edit_list);
                     listView.setAdapter(myadapter);
                     //监听点击事件
                     listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                         @Override
                         public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                            String data= (String) list.get(i);
-                            Intent intent=new Intent(MainActivity.this,input_activity.class);
-                            intent.putExtra("extra_data",data);
-                            list.remove(i);
-                            startActivityForResult(intent,2);
+                            String input_data= (String) text_edit_list.get(i);
+                            Intent intent_list=new Intent(MainActivity.this,text_edit_activity.class);
+                            intent_list.putExtra("extra_data",input_data);
+                            text_edit_list.remove(i);
+                            startActivityForResult(intent_list,2);//打开下一个界面并传入唯一标识符2
                         }
                     });
                     //长安点击事件
@@ -83,9 +73,9 @@ public class MainActivity extends AppCompatActivity {
             case 2:
                 if(resultCode==RESULT_OK){
                     String returndata=data.getStringExtra("data_return");
-                    list.add(returndata);
+                    text_edit_list.add(returndata);
                     ListView listView=(ListView) findViewById(R.id.list_view);
-                    myadapter=new ArrayAdapter<>(MainActivity.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,list);
+                    myadapter=new ArrayAdapter<>(MainActivity.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,text_edit_list);
                     listView.setAdapter(myadapter);
 //                    }
                 }
@@ -95,15 +85,15 @@ public class MainActivity extends AppCompatActivity {
     //长按菜单响应函数
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-        int id=item.getItemId();
+        int item_id=item.getItemId();
         AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-        System.out.println("info:"+menuInfo.position);
-        System.out.println("id"+id);
-        switch (id){
+        switch (item_id) {
             case 0:
-                list.remove(menuInfo.position);
+                text_edit_list.remove(menuInfo.position);
                 myadapter.notifyDataSetChanged();
                 return true;
+            case 1:
+            default:
         }
         return super.onContextItemSelected(item);
     }
